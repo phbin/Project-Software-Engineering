53889b58b953843bf732401f2afc4a0d89b8c96b
20520143 <20520143@gm.uit.edu.vn>
2022-04-30 19:26:24 +0700

    Update

---
+++ QuanLyCuaHangDaQuy/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using QuanLyCuaHangDaQuy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCuaHangDaQuy.Viewmodels
+{
+    public class MainViewModel: BaseViewModel
+    {
+    }
+}
