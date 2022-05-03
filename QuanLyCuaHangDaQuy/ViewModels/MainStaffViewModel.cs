9773c56ab40c67d51459f437b087d241aeb35fb8
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2022-05-03 14:46:52 +0700

    UI ItemsPick - ScrollStyle

---
+++ QuanLyCuaHangDaQuy/ViewModels/MainStaffViewModel.cs
@@ -0,0 +1,41 @@
+﻿using QuanLyCuaHangDaQuy.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyCuaHangDaQuy.ViewModels
+{
+    class MainStaffViewModel:BaseViewModel
+    {
+        ShopItemsClickViewModel ShopItemClickVM { get; set; }
+        public ICommand ShopViewCommand { get; set; }
+        public ICommand ItemClickCommand { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainStaffViewModel()
+        {
+            ShopItemClickVM = new ShopItemsClickViewModel();
+
+            ShopViewCommand = new RelayCommand<ShopWindow>((p) => true, (p) => {
+                CurrentView = this;
+            });
+
+            ItemClickCommand = new RelayCommand<ShopItemsClick>((p) => true, (p) =>
+            {
+                CurrentView = ShopItemClickVM;
+            });
+        }
+    }
+}
---
+++ QuanLyCuaHangDaQuy/ViewModels/ShopItemsClickViewModel.cs
@@ -0,0 +1,32 @@
+﻿using QuanLyCuaHangDaQuy.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QuanLyCuaHangDaQuy.ViewModels
+{
+    class ShopItemsClickViewModel:BaseViewModel
+    {
+        public ICommand ItemClickCommand { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public ShopItemsClickViewModel()
+        {
+            ItemClickCommand = new RelayCommand<ShopItemsClick>((p) => true, (p) => {
+                CurrentView = this;
+            });
+        }
+    }
+}
