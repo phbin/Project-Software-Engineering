4744e9ff4756c09398df906def64e211b6ce1c7c
20520143 <20520143@gm.uit.edu.vn>
2022-05-13 22:03:37 +0700

    Add UI staff_customer, staff_service, database

---
+++ QuanLyCuaHangDaQuy/ViewModels/Staff_AddCustomerVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyCuaHangDaQuy.ViewModels
+{
+    public class Staff_AddCustomerVM : BaseViewModel
+    {
+        public ICommand btnClose { get; set; }
+        public Staff_AddCustomerVM()
+        {
+
+            btnClose = new RelayCommand<Object>(
+                (p) => { return p==null?false:true; },
+                (p) =>
+                {
+                    WindowCloseResult
+                }
+                );
+         
+        }
+    }
+}
