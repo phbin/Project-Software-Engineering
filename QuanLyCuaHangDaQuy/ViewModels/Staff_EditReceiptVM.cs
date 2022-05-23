24d85a844351d57dcbf580ab78049d72cdb14820
20520143 <20520143@gm.uit.edu.vn>
2022-05-23 20:08:22 +0700

    Full code behind

---
+++ QuanLyCuaHangDaQuy/ViewModels/Staff_EditReceiptVM.cs
@@ -0,0 +1,88 @@
+﻿using QuanLyCuaHangDaQuy.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLyCuaHangDaQuy.ViewModels
+{
+    public class Staff_EditReceiptVM :BaseViewModel
+    {
+        
+        public class IN4Customer
+        {
+            private string _fulltName;
+            public string FullName
+            {
+                get { return _fulltName; }
+                set { _fulltName = value; }
+            }
+            private string _id;
+            public string ID
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+        }
+        private string _stt;
+        public string Stt
+        {
+            get { return _stt; }
+            set { _stt = value; OnPropertyChanged(nameof(Stt)); }
+        }
+        private string _iD;
+        public string ID
+        {
+            get { return _iD; }
+            set { _iD = value; OnPropertyChanged(nameof(ID)); }
+        }
+        
+        private string _dateBooking_format;
+        public string DateBooking_format
+        {
+            get { return _dateBooking_format; }
+            set { _dateBooking_format = value; OnPropertyChanged(nameof(DateBooking_format)); }
+        }
+        public ObservableCollection<IN4Customer> ListCustomer { get; set; } = new ObservableCollection<IN4Customer>();
+        private IN4Customer _selectedCustomer;
+        public IN4Customer SelectedCustomer {
+            get { return _selectedCustomer; }
+            set { _selectedCustomer = value; OnPropertyChanged(nameof(SelectedCustomer)); }
+        }
+        public ICommand LoadCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public Staff_EditReceiptVM()
+        {
+            LoadCommand = new RelayCommand<object>(
+            (p) => { return true; },
+            (p) =>
+            {
+                var List = DataProvider.Ins.DB.INFOCUSTOMERs.ToList();
+                foreach (var item in List)
+                {
+                    ListCustomer.Add(new IN4Customer { FullName = item.FullName, ID = item.ID });
+                }
+                //Load
+                ID = Staff_ListReceiptVM.IDD;
+                DateBooking_format= DataProvider.Ins.DB.CUSSERVICEs.ToList().Where(h=>h.ID== Staff_ListReceiptVM.IDD).FirstOrDefault().DateBooking.ToShortDateString();
+                Stt = DataProvider.Ins.DB.CUSSERVICEs.ToList().Where(h => h.ID == Staff_ListReceiptVM.IDD).FirstOrDefault().Stt;
+                string idcs = DataProvider.Ins.DB.CUSSERVICEs.ToList().Where(a => a.ID == Staff_ListReceiptVM.IDD).FirstOrDefault().IDCustomer;
+                SelectedCustomer = ListCustomer.ToList().Where(h => h.ID == idcs).FirstOrDefault();
+            }
+        );
+            EditCommand = new RelayCommand<object>(
+            (p) => { return true; },
+            (p) =>
+            {
+                DataProvider.Ins.DB.CUSSERVICEs.ToList().Where(h => h.ID == Staff_ListReceiptVM.IDD).FirstOrDefault().IDCustomer = SelectedCustomer.ID;
+                DataProvider.Ins.DB.SaveChanges();
+                (p as Window).Close();
+            }
+        );
+        }
+    }
+}
--- QuanLyCuaHangDaQuy/ViewModels/Staff_ListReceiptVM.cs
+++ QuanLyCuaHangDaQuy/ViewModels/Staff_ListReceiptVM.cs
@@ -15,6 +15,7 @@ namespace QuanLyCuaHangDaQuy.ViewModels
 {
     public class Staff_ListReceiptVM : BaseViewModel
     {
+        public static string IDD;
         public ObservableCollection<Receipt> ListReceipt { get; set; } = new ObservableCollection<Receipt>();
         private Receipt _selectedReceipt { get; set; }
         public Receipt SelectedReceipt
@@ -57,54 +58,46 @@ namespace QuanLyCuaHangDaQuy.ViewModels
             }
             if (no >= 2) { SelectedReceipt = ListReceipt[0]; }
         }
+        public void LoadDataAfterTextSearch()
+
+        {
+            if (TextSearch == "" || TextSearch == null)
+            {
+                ListReceipt.Clear();
+                LoadData();
+            }
+            else
+            {
+                ListReceipt.Clear();
+                var List = DataProvider.Ins.DB.CUSSERVICEs.ToList();
+                int no = 1;
+                foreach (var item in List)
+                {
+                    string FullName = DataProvider.Ins.DB.INFOCUSTOMERs.ToList().Where(h => h.ID == item.IDCustomer).FirstOrDefault().FullName;
+                    if (FullName.Contains(TextSearch))
+                    {
+                        ListReceipt.Add(new Receipt
+                        {
+                            No = no,
+                            IDCustomer = item.IDCustomer,
+                            DateBooking_format = item.DateBooking.ToShortDateString(),
+                            FullName = DataProvider.Ins.DB.INFOCUSTOMERs.ToList().Where(h => h.ID == item.IDCustomer).FirstOrDefault().FullName,
+                            DateBooking = item.DateBooking,
+                            ID = item.ID,
+                            Stt = item.Stt,
+                            ColorStt = (item.Stt == "unfinished") ? "#BCBCBC" : "#2CC33B",
+                        });
+                        no++;
+                    }
+                }
+                if (no >= 2) { SelectedReceipt = ListReceipt[0]; }
+            }
+        }
         public ICommand SelectedReceiptCommand { get; set; }
         public ICommand LoadCommand { get; set; }
         public ICommand AddCommand { get; set; }
-        public ICommand LoadEditCommand { get; set; }
         public ICommand TextSearchCommand { get; set; }
-        // EditVM
-        public class IN4Customer
-        {
-            private string _fulltName;
-            public string FullName
-            {
-                get { return _fulltName; }
-                set { _fulltName = value; }
-            }
-            private string _id;
-            public string ID
-            {
-                get { return _id; }
-                set { _id = value; }
-            }
-        }
-        private string _stt;
-        public string Stt
-        {
-            get { return _stt; }
-            set { _stt = value; OnPropertyChanged(nameof(Stt)); }
-        }
-        private string _iD;
-        public string ID
-        {
-            get { return _iD; }
-            set { _iD = value; OnPropertyChanged(nameof(ID)); }
-        }
-        private DateTime _dateBooking;
-        public DateTime DateBooking
-        {
-            get { return _dateBooking; }
-            set { _dateBooking = value; OnPropertyChanged(nameof(DateBooking)); }
-        }
-        private string _dateBooking_format;
-        public string DateBooking_format
-        {
-            get { return _dateBooking_format; }
-            set { _dateBooking_format = value; OnPropertyChanged(nameof(DateBooking_format)); }
-        }
-        public ObservableCollection<IN4Customer> ListCustomer { get; set; } = new ObservableCollection<IN4Customer>();
-        public IN4Customer SelectedCustomer { get; set; }
-        public ICommand FuntionEdit { get; set; }
+      
         public Staff_ListReceiptVM()
         {
             LoadCommand = new RelayCommand<object>(
@@ -120,9 +113,7 @@ namespace QuanLyCuaHangDaQuy.ViewModels
               {
                   Staff_AddReceipt staff_AddReceipt = new Staff_AddReceipt();
                   staff_AddReceipt.ShowDialog();
-                  TextSearch = "";
-                  ListReceipt.Clear();
-                  LoadData();
+                  LoadDataAfterTextSearch();
               }
               );
             SelectedReceiptCommand = new RelayCommand<object>(
@@ -133,8 +124,7 @@ namespace QuanLyCuaHangDaQuy.ViewModels
                 Staff_ListService staff_ListService = new Staff_ListService();
                 staff_ListService.Titlee.Text += IdReceipt;
                 staff_ListService.ShowDialog();
-                ListReceipt.Clear();
-                LoadData();
+                LoadDataAfterTextSearch();
             }
             );
             TextSearchCommand = new RelayCommand<object>(
@@ -179,16 +169,10 @@ namespace QuanLyCuaHangDaQuy.ViewModels
                (p) => { return true; },
                (p) =>
                {
-
-                   //foreach (var item in DataProvider.Ins.DB.INFOCUSTOMERs.ToList())
-                   //{
-                   //    ListCustomer.Add(new IN4Customer { ID = item.ID, FullName = item.FullName });
-                   //}
-                   //staff_EditReceipt.ID.Text = SelectedReceipt.ID;
-                   //staff_EditReceipt.Status.Text = SelectedReceipt.Stt;
-                   //staff_EditReceipt.Bookingdate.Text = SelectedReceipt.DateBooking_format;
-                   //Staff_EditReceipt staff_EditReceipt = new Staff_EditReceipt();
-                   //staff_EditReceipt.ShowDialog();
+                   IDD = SelectedReceipt.ID;
+                   Staff_EditReceipt staff_EditReceipt=new Staff_EditReceipt();
+                   staff_EditReceipt.ShowDialog();
+                   LoadDataAfterTextSearch();
                }
            );
             Receipt.DeleteCommand = new RelayCommand<object>(
@@ -207,18 +191,10 @@ namespace QuanLyCuaHangDaQuy.ViewModels
                       }
                       DataProvider.Ins.DB.CUSSERVICEs.Remove(DataProvider.Ins.DB.CUSSERVICEs.ToList().Where(h => h.ID == SelectedReceipt.ID).FirstOrDefault());
                       DataProvider.Ins.DB.SaveChanges();
-                      ListReceipt.Clear();
-                      LoadData();
-                      TextSearch = "";
+                      LoadDataAfterTextSearch();
                   }
               }
           );
-            FuntionEdit = new RelayCommand<object>(
-            (p) => { return true; },
-            (p) =>
-            {
-            }
-            );
         }
     }
 }
